ecf378e5d6f457ee7eff09d5cc5aad9d1651c46c
Arek Burdach <arek.burdach@gmail.com>
2015-05-29 12:54:57 +0200

    parsowanie ztm

--- src/main/scala/pl/touk/jedzieTramwaj/ZtmDataParser.scala
+++ src/main/scala/pl/touk/jedzieTramwaj/ZtmDataParser.scala
@@ -2,11 +2,63 @@ package pl.touk.jedzieTramwaj
 
 import java.io.InputStream
 
+import scala.io.Source
+
 class ZtmDataParser {
 
-  def parse(stream: InputStream): Seq[BusStop] = ???
+  def parse(stream: InputStream): Seq[BusStop] = {
+    var lastName: String = null
+    var list = List.empty[Option[BusStop]]
+    indnentOnContent(stream).foreach {
+      case (1, content) =>
+        lastName = parseName(content)
+      case (3, content) =>
+        list = parseBusStop(content, lastName) :: list
+      case (4, content) =>
+        val (head :: tail) = list
+          list = head.map(_.add(parseLines(content))) :: tail
+      case _ => // skip
+    }
+    list.flatten.reverse
+  }
 
+  def parseName(content: String): String = {
+    content.substring(10, 43).trim.dropRight(1)
+  }
+
+  def parseBusStop(content: String, name: String): Option[BusStop] = {
+    for {
+      lon <- parseLocationPart(content.substring(112, 126))
+      lat <- parseLocationPart(content.substring(129, 138))
+    } yield {
+      val description = content.substring(34, 68).trim.dropRight(1)
+      val direction = content.substring(75, 109).trim.dropRight(1)
+      BusStop(name, description, direction, Location(lon, lat))
+    }
+  }
+
+  private def parseLocationPart(str: String): Option[Double] = {
+    try {
+      Some(java.lang.Double.parseDouble(str.trim))
+    } catch {
+      case e: NumberFormatException => None
+    }
+  }
+
+  def parseLines(content: String): Seq[String] = {
+    content.substring(40, content.length).trim.split(" +").map(_.replace("\\^", ""))
+  }
+
+  def indnentOnContent(stream: InputStream): Iterator[(Int, String)] = {
+    Source.fromInputStream(stream).getLines().map { line =>
+      val Pattern = "^( *).*".r("indent")
+      line match {
+        case Pattern(indent) => (indent.length/3) -> line
+      }
+    }
+  }
 }
 
-case class BusStop(name: String, direction: String, loc: Localization, lines: List[String])
-case class Localization(lng: Double, lat: Double)
+case class BusStop(name: String, description: String, direction: String, loc: Location, lines: Seq[String] = IndexedSeq.empty) {
+  def add(next: Seq[String]) = copy(lines = lines ++ next)
+}
--- src/test/scala/pl/touk/jedzieTramwaj/ZtmDataParserTest.scala
+++ src/test/scala/pl/touk/jedzieTramwaj/ZtmDataParserTest.scala
@@ -3,9 +3,26 @@ package pl.touk.jedzieTramwaj
 import org.scalatest.{Matchers, FlatSpec}
 
 class ZtmDataParserTest extends FlatSpec with Matchers {
+  import org.scalatest.OptionValues._
 
   it should "parse file" in {
-    new ZtmDataParser().parse(getClass.getResourceAsStream("/ztm.data")) should not be (empty)
+    val result = new ZtmDataParser().parse(getClass.getResourceAsStream("/ztm.data"))
+    result should not be empty
+    result.head.name shouldEqual "KIJOWSKA"
+  }
+
+  it should "found indnents" in {
+    val result = new ZtmDataParser().indnentOnContent(getClass.getResourceAsStream("/ztm.data"))
+    println(result.next())
+  }
+
+  it should "parse name" in {
+    new ZtmDataParser().parseName("   1001   KIJOWSKA,                        --  WARSZAWA") shouldEqual "KIJOWSKA"
+  }
+
+  it should "parse bus stop" in {
+    val content = "         100101   2      Ul./Pl.: TARGOWA,                          Kier.: AL.ZIELENIECKA,                   Y= 52.248670     X= 21.044260"
+    new ZtmDataParser().parseBusStop(content, "foo").value shouldEqual BusStop("foo", "TARGOWA", "AL.ZIELENIECKA", Location(52.248670, 21.044260))
   }
 
 }
