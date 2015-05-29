6b2babc43d785e9b317dfbea0bc595d79592a45e
Maciek Próchniak <mpr@touk.pl>
2015-05-29 09:30:47 +0200

    it compiles

--- src/main/scala/pl/touk/jedzieTramwaj/LocationActor.scala
+++ src/main/scala/pl/touk/jedzieTramwaj/LocationActor.scala
@@ -1,14 +1,20 @@
 package pl.touk.jedzieTramwaj
 
 import java.io.IOException
-import java.util.concurrent.Future
 
 import akka.actor.Actor
+import akka.http.scaladsl.Http
 import akka.http.scaladsl.client.RequestBuilding
-import akka.http.scaladsl.model.{HttpResponse, HttpRequest}
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.http.scaladsl.unmarshalling.Unmarshal
-import akka.stream.scaladsl.{Sink, Source}
-import pl.touk.jedzieTramwaj.MainApp.Result
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import com.typesafe.config.ConfigFactory
+import pl.touk.jedzieTramwaj.MainApp._
+import spray.json.DefaultJsonProtocol
+
+import scala.concurrent.Future
 
 class LocationActor extends Actor {
 
@@ -17,16 +23,30 @@ class LocationActor extends Actor {
 
   }
 
+}
 
-  def telizeRequest(request: HttpRequest): Future[HttpResponse] = Source.single(request).via(telizeConnectionFlow).runWith(Sink.head)
+class LocationFetcher extends DefaultJsonProtocol {
 
-  def fetchIpInfo(ip: String): Future[Either[String, IpInfo]] = {
-    telizeRequest(RequestBuilding.Get(s"/geoip/$ip")).flatMap { response =>
+  val config = ConfigFactory.load()
+
+  lazy val umConnectionFlow: Flow[HttpRequest, HttpResponse, Any] =
+    Http().outgoingConnection(config.getString("services.umHost"), config.getInt("services.umPort"))
+
+  def umRequest(request: HttpRequest): Future[HttpResponse] =
+    Source.single(request).via(umConnectionFlow).runWith(Sink.head)
+
+  implicit val resultFormat1 = jsonFormat2(Value.apply)
+  implicit val resultFormat2 = jsonFormat1(Values.apply)
+  implicit val resultFormat3 = jsonFormat1(TramLocations.apply)
+
+
+  def fetchIpInfo(ip: String): Future[Either[String, TramLocations]] = {
+    umRequest(RequestBuilding.Get(s"/api/action/dbstore_get/?id=11&apikey=${config.getString("apiKey")}")).flatMap { response =>
       response.status match {
-        case OK => Unmarshal(response.entity).to[IpInfo].map(Right(_))
+        case OK => Unmarshal(response.entity).to[TramLocations].map(Right(_))
         case BadRequest => Future.successful(Left(s"$ip: incorrect IP format"))
         case _ => Unmarshal(response.entity).to[String].flatMap { entity =>
-          val error = s"Telize request failed with status code ${response.status} and entity $entity"
+          val error = s"UM request failed with status code ${response.status} and entity $entity"
           logger.error(error)
           Future.failed(new IOException(error))
         }
@@ -35,4 +55,6 @@ class LocationActor extends Actor {
   }
 }
 
-case class
+case class TramLocations(result : Values)
+case class Values(items: List[Value])
+case class Value(key: String, value: String)
